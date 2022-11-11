c276f31e38f338737d383722086d296d257095af
Noela-Murugi <murugin254@gmail.com>
2022-11-11 08:58:31 +0300

    footer struct

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,11 @@ import "../Footer.css";
 
 function Footer() {
   return (
- 
+    <footer className="footerF text-center">
+      <a href="#myPage" title="To Top">
+        <span className="glyphicon glyphicon-chevron-up"></span>
+      </a>
+    </footer>
   );
 }
 export default Footer;
