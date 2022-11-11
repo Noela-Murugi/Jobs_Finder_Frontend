d530ef2013e3935f3b19a60b0e343929ba783bb5
Noela-Murugi <murugin254@gmail.com>
2022-11-11 09:04:07 +0300

    git add about, form, footer

--- src/App.js
+++ src/App.js
@@ -5,6 +5,11 @@ import NavBar from "./navbar/NavBar";
 import Home from "./components/Home";
 import PostForm from "./components/PostForm";
 import Categories from "./components/Categories";
+import About from "./components/About";
+import Footer from "./components/Footer";
+import JobDetails from "./components/JobDetails";
+
+
 
 
 export default App;
