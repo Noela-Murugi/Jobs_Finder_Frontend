d45f7e33ff94fa7e8c5e515da69b3e9dbc6a6e71
Noela-Murugi <murugin254@gmail.com>
2022-11-11 09:02:55 +0300

    git add form and nav imports

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
 import "./App.css";
+import NavBar from "./navbar/NavBar";
+import Home from "./components/Home";
 
 
 export default App;
