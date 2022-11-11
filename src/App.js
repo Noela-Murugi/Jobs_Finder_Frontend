7984acb6cd615812e1f023a6793858cadb234155
Noela-Murugi <murugin254@gmail.com>
2022-11-11 09:03:31 +0300

    git add categories and postform imports

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import { Routes, Route } from "react-router-dom";
 import "./App.css";
 import NavBar from "./navbar/NavBar";
 import Home from "./components/Home";
+import PostForm from "./components/PostForm";
+import Categories from "./components/Categories";
 
 
 export default App;
