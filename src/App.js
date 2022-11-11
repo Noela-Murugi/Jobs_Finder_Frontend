6247fba5605e2797ea31e3565effeeaecf82fce9
WMCharles <charles.wafula@student.moringaschool.com>
2022-11-11 10:58:45 +0300

    remove app.css import

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
-import "./App.css";
 import NavBar from "./navbar/NavBar";
 import Home from "./components/Home";
 import PostForm from "./components/PostForm";
