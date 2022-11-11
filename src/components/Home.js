52a235c009338adc72f43e1568842f205d146e38
Ali Mukhtar <ali.mukhtar@student.moringaschool.com>
2022-11-11 08:57:07 +0300

    complete home folder

---
+++ src/components/Home.js
@@ -0,0 +1,14 @@
+import React, { useState, useEffect } from "react";
+import Landing from "./Landing";
+// import PopularJobs from "./PopularJobs";
+
+const Home = () => {
+  return (
+    <div className="home">
+      <Landing />
+      {/* <PopularJobs/> */}
+    </div>
+  )
+}
+
+export default Home
